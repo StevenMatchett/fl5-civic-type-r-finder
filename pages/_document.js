6365e4fefc7b1d324471d4d4bcc8c5acafe410a9
Steven Matchett <stevenmatchett@Stevens-MacBook-Pro.local>
2022-11-01 17:29:28 -0400

    added ads

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+
+
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4977267667470983"
+     crossorigin="anonymous" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
--- src/Modal.js
+++ src/Modal.js
@@ -21,7 +21,6 @@ export const CivicModal = (props) => {
       // references are now sync'd and can be accessed.
       subtitle.style.color = '#f00';
     }
-    console.log(props.data)
     if (!props.data){
         return null;
     }
