206d704c828c313bf8a905bef475397df6a9de37
Steven Matchett <gosefroba22@gmail.com>
2022-11-17 04:00:29 -0800

    Updates

---
+++ _next/static/EmCK8ruwd-PtdPelGzTA4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/7d0bf13e-f1bf3d45fa73aa2f.js","static/chunks/283-cffaf25a384e6275.js","static/chunks/pages/index-ba2d7f3c7e1708da.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
