821ce4494a1deadd69537ed20d7fa96fd4c4fe5f
Steven Matchett <gosefroba22@gmail.com>
2023-01-30 07:15:52 -0800

    Updates

--- _next/static/5V8PGYyy5vB5vlElSgem7/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/7d0bf13e-f1bf3d45fa73aa2f.js","static/chunks/283-cffaf25a384e6275.js","static/chunks/pages/index-13aad460810f9113.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
